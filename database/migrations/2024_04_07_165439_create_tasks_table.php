298a10007aa7171df3a97e8437ad2416472441be
Elvis <egannem@gmail.com>
2024-04-07 14:40:36 -0300

    feat(database): Make description field nullable in tasks table
    This commit modifies the database schema to allow the description field in the tasks table to be nullable, allowing tasks to be created without a description.

--- database/migrations/2024_04_07_165439_create_tasks_table.php
+++ database/migrations/2024_04_07_165439_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->unsignedBigInteger('relator_user_id');
             $table->boolean('finished')->default(false);
             $table->foreign('relator_user_id')->references('id')->on('users');
