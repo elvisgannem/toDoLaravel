4b363fae22eb9d8d3e30abae7cdf88c7b385f0e6
Elvis <egannem@gmail.com>
2024-04-07 13:58:35 -0300

    feat(database): Add migration and model for tasks
    This commit introduces a migration file to create the 'tasks' table in the database and a corresponding Eloquent model named 'Task' to interact with this table.

---
+++ app/Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'relator_user_id',
+        'finished',
+    ];
+
+    public function relator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'relator_user_id');
+    }
+}
