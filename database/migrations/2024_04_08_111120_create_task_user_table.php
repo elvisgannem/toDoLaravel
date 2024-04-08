01cc23710754edad4d0b5d7cb38c3317bb0ae66a
Elvis <egannem@gmail.com>
2024-04-08 11:04:36 -0300

    feat(database): Add cascade on delete to task_user table
    This commit enhances the database schema by adding cascade on delete to the task_user table. With this feature, when a task is deleted, associated records in the task_user table will be automatically deleted, ensuring data integrity.

--- database/migrations/2024_04_08_111120_create_task_user_table.php
+++ database/migrations/2024_04_08_111120_create_task_user_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('task_id');
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('task_id')->references('id')->on('tasks')->cascadeOnDelete();
             $table->timestamps();
         });
     }
